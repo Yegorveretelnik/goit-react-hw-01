7f3e43787738b6b5b68c87e711fc5a21d5fa8312
yegor <yegorveretelnik@gmail.com>
2025-03-29 13:09:56 +0100

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import React from "react";
+import "./App.css";
+import Profile from "./components/Profile/Profile";
+import userData from "./userData.json";
+import FriendList from "./components/FriendList/FriendList";
+import userData from "./userData.json";
+import friends from "./friends.json";
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+    </>
+  );
+};
+export default App;
